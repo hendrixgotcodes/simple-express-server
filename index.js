4a61c7583bda9dab47ed22a45eabbdec9be0fa80
Samuel O. Asare <asare11samuel@gmail.com>
2022-02-03 15:36:14 +0000

    Handling delete requests

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const path = require('path')
+const exphbs= require('express-handlebars')
 const PORT = process.env.PORT || 5000
 const app = express()
 
 //Body parser middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
+//Handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 
 
 
--- routes/api/members.js
+++ routes/api/members.js
@@ -53,10 +53,34 @@ router.put('/:id', (req, res)=>{
         const updateMember = req.body
         members[req.params.id-1].name = updateMember.name ? updateMember.name : updateMember.email
         members[req.params.id-1].email = updateMember.email ? updateMember.email : updateMember.name
-        res.status(200)
+        res.status(200).json(members[req.params.id-1])
+    }else{
+        res.status(400).json({
+            message: "Bad request"
+        })
     }
     
-    res.json(members[req.params.id-1])
+    
+})
+
+//Delete member
+router.delete('/:id', (req, res)=>{
+
+    if(req.params.id > members.length || req.params.id <=0){
+        res.sendStatus(400).send("Bad request")
+        return
+    }
+
+    const userExist = members.some((member)=>(member.id === parseInt(req.params.id)))
+
+    if(userExist){
+        members.splice(req.params.id-1, 1)
+        res.status(200).json({
+            message: `user ${req.params.id} deleted successfully`
+        })
+    }
+
+
     
 })
 
