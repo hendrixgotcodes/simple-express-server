9e7740a0482ae5ef75eebd18e7966fedab2c945d
Samuel O. Asare <asare11samuel@gmail.com>
2022-02-03 14:13:45 +0000

    middlewares

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const members = require("./Members")
 
 const app = express()
+
+//middleware
+const logger = (req, res, next)=>{
+    console.log('hello from middle ware');
+    next()
+}
+
+//init logger middlewar
+app.use(logger)
+
+app.get('/api/members', (req, res)=>{
+    res.json(members)
+})
 
 app.use(express.static(path.join(__dirname, "public")))
 
