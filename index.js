c3e1f98538fff21c31e77f0a4089ed9599076244
Samuel O. Asare <asare11samuel@gmail.com>
2022-02-03 13:44:24 +0000

    Serving static folder

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const express = require('express')
 const path = require('path')
-
-const app = express()
 const PORT = process.env.PORT || 5000
 
-app.get("/", (req, res)=>{
+const app = express()
 
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-    
-})
+app.use(express.static(path.join(__dirname, "public")))
 
 app.listen(PORT, ()=>console.log(`Server started on port: ${PORT}`))
