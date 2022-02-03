263cc4ca56644810c361fdf14868f27f1dc7c581
Samuel O. Asare <asare11samuel@gmail.com>
2022-02-03 14:43:53 +0000

    Routing similar urls

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const members = require("./Members")
-
 const app = express()
 
-//middleware
-const logger = (req, res, next)=>{
-    console.log('hello from middle ware');
-    next()
-}
 
-//init logger middlewar
-app.use(logger)
 
-app.get('/api/members', (req, res)=>{
-    res.json(members)
-})
+
+
 
 app.use(express.static(path.join(__dirname, "public")))
+
+//memebers api route
+app.use('/api/members', require('./routes/api/members'))
 
 app.listen(PORT, ()=>console.log(`Server started on port: ${PORT}`))
